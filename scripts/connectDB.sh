ba37f4e8ddc5197cd8a7a0388fcda6e05a8807ca
Mahmoud Osama <Mahmoud@localhost.localdomain>
2025-12-25 20:22:50 +0200

    Feat: project structure

---
+++ scripts/connectDB.sh
@@ -0,0 +1,53 @@
+#!/bin/bash 
+
+clear
+PS3="Choose an option: "
+select choice in \
+	"Create Table" \
+       	"List Tables" \
+	"Drop Table" \
+	"Insert into Table" \
+	"Select From Table" \
+	"Delete From Table" \
+	"Update table" \
+	"Exit"
+do
+	case "$REPLY" in 
+		1)
+			./CreateTable.sh
+			;;
+		
+		2)
+			./ListTables.sh
+			;;
+
+		3)
+			./DropTable.sh
+			;;
+
+		4)
+			./InsertintoTable.sh
+			;;
+
+		5)
+			./SelectfromTable.sh
+			;;
+
+		6)
+			./DeletefromTable.sh
+			;;
+
+		7)
+			./UpdateTable.sh
+			;;
+
+		8)
+			exit
+			;;
+
+		*)
+			echo "Invalid choice"
+			;;
+
+	esac
+done	
